876c3b91251d24e847bbe3d8f141d574deed0d46
Job <tingjun.li.job@gmail.com>
2025-11-19 15:03:16 +0800

    project init

---
+++ application/extra/addons.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'autoload' => false,
+    'hooks' => [
+        'app_init' => [
+            'fzly',
+        ],
+        'upgrade' => [
+            'fzly',
+        ],
+    ],
+    'route' => [],
+    'priority' => [],
+    'domain' => '',
+];
